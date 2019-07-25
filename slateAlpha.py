ac4a5608c675fe3e38f6a86658a423c49cfedd20
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-07-25 16:01:57 -0400

    Update slateAlpha.py
    • updated the exit,! command
    • now you can type in exit,! anywhere except the text input part
    • bumped version

--- slateAlpha.py
+++ slateAlpha.py
@@ -1,40 +1,36 @@
 #Experimento!  
 #SLATE text editor
-#Version 0.2.3c
-print("HELLLLLLLLLO GitHub! ")
+#Version 0.2.4
 print("Going to save files in directory _shell.py is in. If you want to exit, type exit,!")
-print("slate version 0.2.3c (ALPHA)")
+print("slate version 0.2.4 (ALPHA)")
 while True:
     mode = input("Mode: a for append, w for overwrite (or newfile), l for newline. Case-sensitive. ")
     if mode == "a":
         print("Mode: Append (A)")
         file = input("Filename?")
         if file == "exit,!":
-            print("Not developed yet")
-            #Shell()
+            exit()
         else:
             my_file = open(file, "a") #open user file as append
             my_file.write(input("Type a line. ")) #Append to the file
-                
-
     elif mode == "w":
-        print("Mode: Overwrite or newfile (W)")
+        print("Mode: Overwrite or newfile (w)")
         file = input("Enter filename: ")
         if file == "exit,!":
-            print("If you did this in the actual version it would restart the shell.")
-            #Shell()
+            exit()
         else:
-                my_file = open(file, "w")  #open user-chosen file as readwrite
+                my_file = open(file, "W")  #open user-chosen file as readwrite
                 my_file.write(input("Type a line. ")) #Write to the file
     elif mode == "l":
         print("Mode: Newline (L)")
         file = input("Filename?")
         if file == "exit,!":
-            print("Not developed yet")
-            #Shell()
+            exit()
         else:
             my_file = open(file, "a") #open user file as append
             my_file.write('''
-            ''')) # Add new line?  to the file
+            ''')) # Add new line? to the file
+    elif mode == "exit,!":
+        exit()
     else: 
-        print("Well... you need to type a(ppend), or (over)w(rite), or (new)l(ine) To exit, type either a, w, or l. Then type exit,!")
+        print("Command not found; you need to type a(ppend), or (over)w(rite), or (new)l(ine) To exit, exit,!. Sorry for the inconvenience")
