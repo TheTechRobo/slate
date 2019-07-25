8ade7a72743da0cb029220a8ce6d484f62c9d3bf
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-07-25 16:11:19 -0400

    Update slateAlpha.py
    • clear screen every time there is more input
    • fake loading time to make you able to read the text before the screen is cleared
    • bumped version

--- slateAlpha.py
+++ slateAlpha.py
@@ -1,19 +1,32 @@
 #Experimento!  
 #SLATE text editor
-#Version 0.2.4
-print("Going to save files in directory _shell.py is in. If you want to exit, type exit,!")
-print("slate version 0.2.4 (alpha)")
+#Version 0.2.5
+#setup
+import time
+print("Going to save files in directory slateAlpha.py is in. If you want to exit, type exit,!")
+print("slate version 0.2.5 (alpha)")
+print("Loading................")
+time.sleep(8)
+print("Done.")
 while True:
+    for i in range(0, 11):
+            print()
     mode = input("Mode: a for append, w for overwrite (or newfile), l for newline, exit,! for exit. Case-sensitive. ")
     if mode == "a":
+        for i in range(0, 11):
+            print()
         print("Mode: Append (a)")
         file = input("Filename?")
         if file == "exit,!":
             exit()
         else:
             my_file = open(file, "a") #open user file as append
+            for i in range(0, 11):
+            print()
             my_file.write(input("Type a line. ")) #Append to the file
     elif mode == "w":
+        for i in range(0, 11):
+            print()
         print("Mode: Overwrite or newfile (w)")
         file = input("Enter filename: ")
         if file == "exit,!":
@@ -22,6 +35,8 @@ while True:
                 my_file = open(file, "W")  #open user-chosen file as readwrite
                 my_file.write(input("Type a line. ")) #Write to the file
     elif mode == "l":
+        for i in range(0, 11):
+            print()
         print("Mode: Newline (l)")
         file = input("Filename?")
         if file == "exit,!":
@@ -33,4 +48,6 @@ while True:
     elif mode == "exit,!":
         exit()
     else: 
+        for i in range(0, 11):
+            print()
         print("Command not found; you need to type a(ppend), or (over)w(rite), or (new)l(ine) To exit, exit,!. Sorry for the inconvenience")
