7a637918f60726de79bb809089bbfb84ae019810
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-04-17 17:54:38 -0400

    Changes.

--- slateAlpha.py
+++ slateAlpha.py
@@ -1,27 +1,27 @@
 #SLATE is a Eureka
 #SLATE text editor
-#Version 0.3.1
+#Version 0.3.x. 
+#In early development, but has rare updates.
 #setup
 from sys import exit
-print("Going to save files in current working directory (typically ~). If you want to exit, type exit,!")
-print("slate version 0.3.1 (alpha)")
+print("Going to save files in current working directory (typically ~/slate). If you want to exit, type exit,!")
 print("Loading................")
 from time import *
 import os
+def wait():
+    sleep(1.5)
+
 def append():
     os.system('clear')
     print("Mode: Append (a)")
     file = input("Filename?")
-    if file == "exit,!":
+    if file.lower() == "exit,!":
     	exit()
-	elif file == "EXIT,!":
-		exit()
     else:
-        my_file = open(file, "a") #open user file as append
+        my_file = open(file, "a+") #open user file as append or, if it doesn't exist, create it
         os.system('clear')
         my_file.write(input("Type a line. ")) #Append to the file
     print()
-
 
 def overwrite():
     os.system('clear')
@@ -32,7 +32,7 @@ def overwrite():
     elif file == "EXIT,!":
         exit()
     else:
-        my_file = open(file, "w")  #open user-chosen file as readwrite
+        my_file = open(file, "w+")  #open user-chosen file as readwrite
         my_file.write(input("Type a line. ")) #Write to the file
 		
 def newline():
@@ -42,11 +42,10 @@ def newline():
     if file == "exit,!":
         exit()
     else:
-        my_file = open(file, "a") #open user file as append
+        my_file = open(file, "a+") #open user file as append
         my_file.write('''
         ''') # Add new line? to the file
-def nothing():
-	pass
+wait()
 print("Done.")
 while True:
     os.system('clear')
@@ -69,8 +68,8 @@ while True:
         os.system('clear')
         print("Command not found; you need to type a(ppend), or (over)w(rite), or (new)l(ine) To exit, exit,!. Sorry for the inconvenience")
         print("Please wait...")
-        sleep(1.5)
+        wait()
 # No More If Statements
     os.system('clear')
-    print("Please Wait")
-    sleep(1.5)
+    print("Please wait...")
+    wait()
