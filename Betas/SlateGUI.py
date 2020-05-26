4af666aa0fd2324d8563557f7c0963139a320a8f
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2020-05-26 14:13:36 -0400

    moved things around

--- Betas/SlateGUI.py
+++ Betas/SlateGUI.py
@@ -25,7 +25,10 @@ scrollbar.pack( side = RIGHT, fill = Y ) #vertical
 text = Text(slate, yscrollcommand=scrollbar.set)
 text.pack()
 scrollbar.config( command = text.yview )
-Button(slate, text="Save", command=save).pack()
-Button(slate, text="Open", command=openFile).pack()
+w = Button(slate, text="Save", command=save)
+w2 = Button(slate, text="Open", command=openFile)
+
+w.pack(padx=5, pady=10, side=LEFT)
+w2.pack(padx=5, pady=10, side=LEFT)
 
 mainloop()
