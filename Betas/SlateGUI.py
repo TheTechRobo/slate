4d2387925c43fc913c42938a750cfcc753d5e432
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2020-05-26 08:40:00 -0400

    WIP

--- Betas/SlateGUI.py
+++ Betas/SlateGUI.py
@@ -1,16 +1,17 @@
 from tkinter import *
 
 slate = Tk() #set up window. `slate' is now the name of the window, internally
-scrollbar = Scrollbar(slate) #set up scrollbar
-scrollbarX = Scrollbar(slate) #set up horizontal scrollbar
+scrollbar = Scrollbar(slate, orient=VERTICAL) #set up scrollbar
+scrollbarX = Scrollbar(slate, orient=HORIZONTAL) #set up horizontal scrollbar
 scrollbar.pack( side = RIGHT, fill = Y ) #vertical
-scrollbarX.pack(side=RIGHT, fill=Y) #horizontal
+scrollbarX.pack(side=BOTTOM, fill=X) #horizontal
 
 mylist = Listbox(slate, yscrollcommand=scrollbar.set, xscrollcommand=scrollbarX.set)
-for line in range(100):
-   mylist.insert(END, "This is line number " + str(line))
+for line in range(100000):
+   mylist.insert(END, "This is line number:::::::::::::::: " + str(line))
 
 mylist.pack( side = LEFT, fill = BOTH )
 scrollbar.config( command = mylist.yview )
+scrollbarX.config(command=mylist.yview)
 
 mainloop()
